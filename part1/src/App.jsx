c0de2083415b15f1d3fe30042fafc9eb2e465011
shriyapingle <shriyapingle@gmail.com>
2026-03-31 11:08:42 +0200

    exercises 1.6-1.11

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,68 +1,115 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.name.name}</h1>
-    </div>
-  )
-}
+import { useState } from 'react'
+import Button from './Button'
+import Display from './Display'
 
-const Content = (props) => {
-  props = props.parts.parts
-  return (
-    <>
-      <Part name={props[0].name} exercise={props[0].exercises} />
-      <Part name={props[1].name} exercise={props[1].exercises} />
-      <Part name={props[2].name} exercise={props[2].exercises} />
-    </>
-  )
+const Statistics = ({ props }) => {
+  if (props.length === 0) {
+    return <div>No feedback given</div>
+  } else {
+
+
+    const list = props.map((e,i) => <StatisticLine name={e.name} value={e.value} key={i} />)
+    return <table><tbody>{list}</tbody></table>
+
+  }
+
 
 }
 
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.name} {props.exercise}
-      </p>
-    </>
-  )
-}
-
-const Total = (props) => {
-  props = props.parts.parts;
-  return (
-    <>
-      <p>Number of exercises {props[0].exercises + props[1].exercises + props[2].exercises}</p>
-    </>
-  )
+const StatisticLine = ({ name, value }) => {
+  if (name == 'Positive' && value > 0) {
+    return (
+      <tr>
+        <td>{name}</td><td>{Number.isInteger(value) ? value : Number.parseFloat(value).toFixed(1)} %</td>
+      </tr>
+    )
+  }
+  return <tr><td>{name}</td><td>{Number.isInteger(value) ? value : Number.parseFloat(value).toFixed(1)}</td></tr>
 }
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positivePercent, setPositivePercent] = useState(0)
+  const [statsArray, setStatsArray] = useState([])
+  const handleGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    handleStatistics(updatedGood, neutral, bad)
+  }
+
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    handleStatistics(good, updatedNeutral, bad)
+  }
+
+  const handleBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    handleStatistics(good, neutral, updatedBad)
+  }
+
+  const handleStatistics = (statGood, statNeutral, statBad) => {
+    const statTotal = statBad + statGood + statNeutral
+    setTotal(statTotal)
+    const statAverage = ((statGood * 1) + (statBad * -1)) / statTotal
+    setAverage(statAverage)
+    const statPositive = (statGood / statTotal) * 100
+    setPositivePercent(statPositive)
+
+    let statArray = [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: 'Good',
+        value: statGood
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: 'Neutral',
+        value: statNeutral
       },
       {
-        name: 'State of a component',
-        exercises: 14
+        name: 'Bad',
+        value: statBad
+      },
+      {
+        name: 'All',
+        value: statTotal
+      },
+      {
+        name: 'Average',
+        value: statAverage
+      },
+      {
+        name: 'Positive',
+        value: statPositive
       }
     ]
+    setStatsArray(statArray)
+
   }
 
   return (
     <div>
-      <Header name={course} />
-      <Content parts={course} />
-      <Total parts={course} />
-    </div >
+
+      <Display text="Give Feedback" />
+      <br></br>
+      <div>
+        <Button onClick={handleGood} text="Good" /> {' '}
+        <Button onClick={handleNeutral} text="Netutral" /> {' '}
+        <Button onClick={handleBad} text="Bad" />
+      </div>
+      <br></br>
+      <br></br>
+      <Display text="Statistics" />
+      <br></br>
+      <Statistics props={statsArray} />
+
+    </div>
   )
 }
 
