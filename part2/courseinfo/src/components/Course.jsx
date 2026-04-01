6d434fcc52a5ceafdbf54881ca192371eaea77ca
shriyapingle <shriyapingle@gmail.com>
2026-04-01 21:09:27 +0200

    removed console

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,24 +1,23 @@
-const CourseDetails = ({course}) => {
-    console.log(course)
+const CourseDetails = ({ course }) => {
     const courseParts = course.parts
     return (
         <div>
             <h3>{course.name}</h3>
-            <div>{courseParts.map(exe=><div key={exe.id}>{exe.name} {exe.exercises}</div>)}</div>
-            <div>total of {courseParts.reduce((sum,exer)=>{return sum+exer.exercises},0)} exercises</div>
+            <div>
+                {courseParts.map(exe => <div key={exe.id}>{exe.name} {exe.exercises}</div>)}
+                <h4>total of {courseParts.reduce((sum, exer) => { return sum + exer.exercises }, 0)} exercises</h4>
+            </div>
         </div>
     )
 }
 
 
 const Course = ({ course }) => {
-    console.log(course)
-    course.map(ele => console.log(ele.name))
     // const courseDetails = course.map(ele => <h1>{ele.name}</h1>)
     return (
         <div>
             <h1> Web development curriculum</h1>
-            <div>{course.map(ele => <CourseDetails course={ele} key={ele.id}/>)}</div>
+            <div>{course.map(ele => <CourseDetails course={ele} key={ele.id} />)}</div>
         </div>
     )
 }
