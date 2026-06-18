4a519314c7d27e22bdd5441c68bb94d93009ffc3
shriyapingle <shriyapingle@gmail.com>
2026-06-18 16:22:14 +0200

    notes exercises

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import Note from './components/Notes'
 import axios from 'axios'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
 const App = () => {
 
@@ -9,6 +11,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [showAll, setShowAll] = useState(true)
   const [newNote, setNewNote] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then(initialNotes => { setNotes(initialNotes) })
@@ -43,9 +46,12 @@ const App = () => {
     noteService.update(changedNote.id, changedNote).then(resp => {
       setNotes(notes.map(note => note.id === id ? resp.data : note))
     }).catch(err => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
+      setErrorMessage(
+        `Note '${note.content}' was already removed from server`
       )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
       setNotes(notes.filter(n => n.id !== id))
     })
   }
@@ -53,6 +59,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -67,6 +74,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer/>
     </div>
   )
 }
--- part2/notes/src/components/Notes.jsx
+++ part2/notes/src/components/Notes.jsx
@@ -1,7 +1,8 @@
 const Note = ({ note, toggleImportance }) => {
+  
   const label = note.important ? 'Mark not important' : 'Mark important'
   return (
-    <li>
+    <li className="note">
       {note.content}  <button onClick={toggleImportance}>{label}</button>
     </li>
   )
