900f7fc1a2802cb740ddad2c3ea2a88dd44b784c
shriyapingle <shriyapingle@gmail.com>
2026-06-18 16:21:55 +0200

    phonebook steps 11&12: exercises 2.16&2.17

--- part2/phonebook/src/services/phoneBook.js
+++ part2/phonebook/src/services/phoneBook.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3002/persons'
 
 const getAll = () => {
     const result = axios.get(baseUrl)
